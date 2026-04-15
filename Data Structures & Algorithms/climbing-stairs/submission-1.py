bf944f6fe59ecbf02af5523ae4ac8b0eebe9b931
Mubashir21 <91730159+Mubashir21@users.noreply.github.com>
2026-04-15 21:42:18 +0300

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        left, right = 1, 1
+
+        for _ in range(n - 1):
+            temp = right
+            right = left + right
+            left = temp
+        return right
