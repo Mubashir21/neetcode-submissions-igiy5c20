e0c5d16d6f79d23e90f834b4811752968b2ce07c
Mubashir Shoukat <mubashirshoukat@gmail.com>
2026-05-21 18:22:39 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # maps = defaultdict(int)
+
+        # for num in nums:
+        #     maps[num] = 1 + maps[num]
+        maps = Counter(nums)
+
+        tups = []
+        for key, value in maps.items():
+            tups.append((value, key))
+        tups.sort(reverse=True)
+        
+        return [tups[i][1] for i in range(k)]
